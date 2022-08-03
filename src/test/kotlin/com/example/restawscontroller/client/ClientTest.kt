6a54de520af48c5e80169a21ffee69a753822996
Christian Wollmann <christian.wollmann@gmx.net>
2022-08-03 14:23:26 +0200

    test: save some progress

--- src/test/kotlin/com/example/restawscontroller/client/ClientTest.kt
+++ src/test/kotlin/com/example/restawscontroller/client/ClientTest.kt
@@ -2,14 +2,18 @@ package com.example.restawscontroller.client
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
-import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.*
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
+import reactor.kotlin.test.test
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(initializers = [WireMockContextInitializer::class])
@@ -17,28 +21,51 @@ import org.springframework.test.context.ContextConfiguration
 internal class ClientTest(
     @Autowired val client: Client
 ) {
-    private lateinit var wireMockServer: WireMockServer
+    @Autowired private lateinit var wireMockServer: WireMockServer
 
     @AfterEach
     fun afterEach() {
         wireMockServer.resetAll()
     }
 
-    @Test fun shouldReturnValidStatusCode(){
+    /*
+    @ParameterizedTest
+    @MethodSource("ranges")
+    fun shouldReturnValidStatusResponse(input: Int, expected: String) {
+
         wireMockServer.stubFor(
-            WireMock.get("/ip/")
+            WireMock.get("/response")
                 .willReturn(
                     aResponse()
                         .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-                        .withBody("[]")
+                        .withStatus(input)
                 )
         )
 
         client
-            .getResponseFromApi("https://ip-ranges.amazonaws.com/ip-ranges.json")
+            .getStatusCode("${wireMockServer.baseUrl()}/response")
             .test()
+            .expectNext(expected)
             .verifyComplete()
+
+        verify(getRequestedFor(urlEqualTo("/response")))
     }
+
+    companion object {
+
+        @JvmStatic
+        fun ranges() = listOf(
+            200..299 to "Seite erreichbar",
+            300..399 to "Weiterleitung",
+            400..499 to "Seite erreichbar (Client error - Code: %s)",
+            500..599 to "Seite nicht erreichbar (Server error - Code: %s)",
+        )
+            .flatMap { (range, string) ->
+                range.map { Arguments.of(it, string.format(it)) }
+            }
+    }
+
+     */
 
     @Test
     fun main() {
