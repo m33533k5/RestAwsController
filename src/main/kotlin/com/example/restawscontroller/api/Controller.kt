84979a6f12571fa21d723fe168baa023386e122c
Christian Wollmann <christian.wollmann@gmx.net>
2022-08-03 08:57:23 +0200

    refactor: Output of IPs with a list

--- src/main/kotlin/com/example/restawscontroller/api/Controller.kt
+++ src/main/kotlin/com/example/restawscontroller/api/Controller.kt
@@ -2,9 +2,6 @@ package com.example.restawscontroller.api
 
 import com.example.restawscontroller.client.Client
 import com.example.restawscontroller.data.AwsIpData
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,15 +16,13 @@ class Controller(
     //http://localhost:8081/ip/?region=ALL
     @RequestMapping("/")
     fun getUrlContent(@RequestParam region: String): ResponseEntity<String> {
-        var response: AwsIpData? = client.main(region = region)
-        if (response != null) {
-            client.getPossibleIps(response)
-            client.getPossibleIpv6Ips(response)
-        };
+        val response: AwsIpData = client.main(region = region)
+        client.getPossibleIps(response)
+        client.getPossibleIpv6Ips(response)
+
         return when(getDataIsEmpty(response)){
             true -> ResponseEntity<String>("Keine Ergebnisse gefunden", HttpStatus.OK)
-            false -> for (item in client.ips) ResponseEntity(item.toString(), HttpStatus.OK)
-            //false -> ResponseEntity<String>(client.ips.forEach{e -> ResponseEntity<String>(e,HttpStatus.OK)}.toString(), HttpStatus.OK)
+            false -> ResponseEntity<String>(client.ips.toString(), HttpStatus.OK)
         }
 
     }
