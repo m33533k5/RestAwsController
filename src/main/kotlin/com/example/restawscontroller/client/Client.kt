7127dc0d2e0a81be976dedce89fdfb3a556535a6
Christian Wollmann <christian.wollmann@gmx.net>
2022-08-03 09:39:18 +0200

    refactor: Reduce the complexity of functions to facilitate testing.

--- src/main/kotlin/com/example/restawscontroller/client/Client.kt
+++ src/main/kotlin/com/example/restawscontroller/client/Client.kt
@@ -4,6 +4,7 @@ import com.example.restawscontroller.data.AwsIpData
 import com.example.restawscontroller.data.Ipv6Prefixe
 import com.example.restawscontroller.data.Prefixe
 import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.stereotype.Component
 import java.net.URL
@@ -15,7 +16,55 @@ class Client {
 
     fun main(region: String): AwsIpData {
         val rawIpData = getDataFromAPI()
+        return createAwsIpData(region, rawIpData)
+    }
 
+    private fun getDataFromAPI(): AwsIpData {
+        val response = getResponseFromApi()
+
+        val jsonNodeList = createJacksonMapper(response)
+
+        return AwsIpData(
+            createDate = jsonNodeList.findValue("createDate").textValue(),
+            ipv6Prefixes = getIpv6Prefixes(jsonNodeList),
+            prefixes = getIpPrefixes(jsonNodeList),
+            syncToken = jsonNodeList.findValue("syncToken").textValue(),
+        )
+    }
+
+    private fun getResponseFromApi(): String {
+        return URL("https://ip-ranges.amazonaws.com/ip-ranges.json").readText()
+    }
+
+    private fun createJacksonMapper(response: String): JsonNode {
+        val mapper = jacksonObjectMapper()
+        mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)
+        return mapper.readTree(response)
+    }
+
+    private fun getIpv6Prefixes(jsonNodeList: JsonNode): List<Ipv6Prefixe> {
+        return jsonNodeList.findValue("ipv6_prefixes").map { ipv6_prefixesNode ->
+            Ipv6Prefixe(
+                ipv6Prefix = ipv6_prefixesNode.get("ipv6_prefix").textValue(),
+                region = ipv6_prefixesNode.get("region").textValue(),
+                service = ipv6_prefixesNode.get("service").textValue(),
+                networkBorderGroup = ipv6_prefixesNode.get("network_border_group").textValue(),
+            )
+        }
+    }
+
+    private fun getIpPrefixes(jsonNodeList: JsonNode): List<Prefixe> {
+        return jsonNodeList.findValue("prefixes").toList().map { prefixesNode ->
+            Prefixe(
+                ipPrefix = prefixesNode.get("ip_prefix").textValue(),
+                region = prefixesNode.get("region").textValue(),
+                service = prefixesNode.get("service").textValue(),
+                networkBorderGroup = prefixesNode.get("network_border_group").textValue(),
+            )
+        }
+    }
+
+    private fun createAwsIpData(region: String, rawIpData: AwsIpData): AwsIpData {
         return when (region) {
             "ALL" -> rawIpData
             else -> {
@@ -27,39 +76,6 @@ class Client {
                 )
             }
         }
-    }
-
-    private fun getDataFromAPI(): AwsIpData {
-        val response: String = URL("https://ip-ranges.amazonaws.com/ip-ranges.json").readText()
-
-        val mapper = jacksonObjectMapper()
-        mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)
-        val jsonNodeList = mapper.readTree(response)
-
-        val ipv6Prefixes = jsonNodeList.findValue("ipv6_prefixes").map { ipv6_prefixesNode ->
-            Ipv6Prefixe(
-                ipv6Prefix = ipv6_prefixesNode.get("ipv6_prefix").textValue(),
-                region = ipv6_prefixesNode.get("region").textValue(),
-                service = ipv6_prefixesNode.get("service").textValue(),
-                networkBorderGroup = ipv6_prefixesNode.get("network_border_group").textValue(),
-            )
-        }
-
-        val ipPrefixe = jsonNodeList.findValue("prefixes").toList().map { prefixesNode ->
-            Prefixe(
-                ipPrefix = prefixesNode.get("ip_prefix").textValue(),
-                region = prefixesNode.get("region").textValue(),
-                service = prefixesNode.get("service").textValue(),
-                networkBorderGroup = prefixesNode.get("network_border_group").textValue(),
-            )
-        }
-
-        return AwsIpData(
-            createDate = jsonNodeList.findValue("createDate").textValue(),
-            ipv6Prefixes = ipv6Prefixes,
-            prefixes = ipPrefixe,
-            syncToken = jsonNodeList.findValue("syncToken").textValue(),
-        )
     }
 
     private fun getIpv6RegionByFilter(rawIpData: AwsIpData, region: String): List<Ipv6Prefixe> {
@@ -91,5 +107,4 @@ class Client {
         getPossibleIpv6Ips(rawIpData)
         return listOfAllIps
     }
-
 }
