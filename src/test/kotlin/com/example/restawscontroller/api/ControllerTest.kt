706224039ce90542e446142f1aab2e5a1e1e9f0f
Christian Wollmann <christian.wollmann@gmx.net>
2022-08-04 09:29:50 +0200

    test: create test class for controller

---
+++ src/test/kotlin/com/example/restawscontroller/api/ControllerTest.kt
@@ -0,0 +1,16 @@
+package com.example.restawscontroller.api
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ControllerTest {
+
+    @Test
+    fun getUrlContent() {
+    }
+
+    @Test
+    fun responseDataIsEmpty() {
+    }
+}
