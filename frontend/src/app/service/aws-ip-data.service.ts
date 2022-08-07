c261cb24de178d4120375290be7d6ea42741a42c
Christian Wollmann <christian.wollmann@gmx.net>
2022-08-08 00:11:58 +0200

    refactor: create fimctopm for getting all data

--- frontend/src/app/service/aws-ip-data.service.ts
+++ frontend/src/app/service/aws-ip-data.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {AwsIpData} from "../awsIpData";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AwsIpDataService {
 
-  constructor() { }
+  private dataUrl: string = "http://localhost:8081/ip/?region=ALL"
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  findAll(): Observable<AwsIpData[]>{
+    return this.httpClient.get<AwsIpData[]>(this.dataUrl)
+  }
 }
